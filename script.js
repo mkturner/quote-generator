b8937f743279837a36d26d5af015b5200d534478
Marvin T <contact@marvinkturner.com>
2020-08-04 11:20:53 -0400

    adding functionality to get quotes from API

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-// let corsProxyServer = https://gentle-plains-15721.herokuapp.com/ ;
-
 console.log('testing')
+
+// Get Quote from API
+async function getQuote() {
+    const corsProxyUrl = 'https://gentle-plains-15721.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+    try {
+        // Wait until data comes back, then set to variable to process
+        const response = await fetch(corsProxyUrl + apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log('Whoops, no quote...', error);
+        getQuote();
+    }
+}
+
+// Run when page loads
+getQuote();
