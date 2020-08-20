b3340a5f9b8c849c1c5316f487653422d2936a44
Marvin T <contact@marvinkturner.com>
2020-08-19 23:22:26 -0400

    feat: add loading animation

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-console.log('testing')
+/* 
+    Hide content container
+    Show loading animation
+    Fetch data from API
+    Manipulate Dom to insert data from API
+    Show content container
+    Hide loading animation
+*/
 
 // Target IDs to dynamically fill content
 const quoteContainer = document.getElementById('quote-container');
@@ -6,9 +13,32 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterButton = document.getElementById('twitter');
 const newQuoteButton = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show loading animation
+function showLoadingAnimation() {
+    /* This function hides the content box and shows a loading animation 
+        until the promise in the api fetch() is finished. This provides 
+        some context to the user that work is still being done. This is 
+        especially helpful when the CORS proxy is warming up from a 
+        cold start.
+    */
+    loader.hidden = false;
+    quoteContainer.hidden = true; // hide until ready
+}
+
+function hideLoadingAnimation() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get Quote from API
 async function getQuote() {
+    // Show loading animation until content ready.
+    showLoadingAnimation();
+
     const corsProxyUrl = 'https://gentle-plains-15721.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
@@ -33,6 +63,9 @@ async function getQuote() {
         }
 
         quoteText.innerText = data.quoteText;
+
+        // When done loading, show quote
+        hideLoadingAnimation();
     } catch (error) {
         await getQuote();
     }
